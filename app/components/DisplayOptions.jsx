c8e73a7a3e0cc2c93f00fe3bf2c9c8335a0329b3
kapilrc <kapilrc@gmail.com>
2024-02-06 02:14:07 +0530

    Display Type grid/list component added

---
+++ app/components/DisplayOptions.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import ViewListIcon from '@mui/icons-material/ViewList';
+import ViewModuleIcon from '@mui/icons-material/ViewModule';
+
+const DisplayOptions = ({ value, onChange }) => {
+  console.log(value);
+  return (
+    <Stack direction="row" justifyContent="space-between" spacing={1}>
+      <IconButton onClick={(e) => onChange(e, "list")}>
+        <ViewListIcon color={value === "list" ? "primary": "inherit"} />
+      </IconButton>
+
+      <IconButton onClick={(e) => onChange(e, "grid")}>
+        <ViewModuleIcon color={value === "grid" ? "primary": "inherit"} />
+      </IconButton>
+    </Stack>
+    // <ToggleButtonGroup value={value} onChange={onChange}>
+    //     <ToggleButton value="list">
+    //       <ViewListIcon />
+    //     </ToggleButton>
+    //     <ToggleButton value="grid">
+    //       <ViewModuleIcon />
+    //     </ToggleButton>
+    // </ToggleButtonGroup>
+  )
+}
+
+export default DisplayOptions
